1e38733a5a55ea5fd1118da38e872a7890474ed3
Wesley <wesleylondonwl@gmail.com>
2018-07-24 21:06:20 -0400

    added start of music selection modal

---
+++ js/interfaces.js
@@ -0,0 +1,26 @@
+
+var filebutton = document.getElementById("musicfile");
+var custombutton = document.getElementById("custom-upload");
+
+//music selection modal
+var musicmodal = document.getElementById("music-modal");
+var musicselectionbtn = document.getElementById("musicselectionbtn");
+
+
+
+musicselectionbtn.onclick = function(){
+    musicmodal.style.display = "block";
+}
+
+
+window.onclick = function(){
+    if (event.target == musicmodal) {
+       musicmodal.style.display = "none";
+   }
+}
+
+
+
+custombutton.addEventListener("click", function(){
+    filebutton.click();
+})
--- js/uploadbutton.js
+++
@@ -1,8 +0,0 @@
-//Custom upload button magic
-
-var filebutton = document.getElementById("musicfile");
-var custombutton = document.getElementById("custom-upload");
-
-custombutton.addEventListener("click", function(){
-    filebutton.click();
-})
