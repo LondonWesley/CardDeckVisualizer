8e89c0973e40d97a243c19011ed45144979fae56
Wesley <wesleylondonwl@gmail.com>
2018-08-03 21:50:51 -0400

    added slider for volume in settings. (does not function)

--- js/interfaces.js
+++ js/interfaces.js
@@ -1,4 +1,4 @@
-
+//upload button
 var filebutton = document.getElementById("musicfile");
 var custombutton = document.getElementById("custom-upload");
 
@@ -6,10 +6,16 @@ var custombutton = document.getElementById("custom-upload");
 var musicmodal = document.getElementById("music-modal");
 var musicselectionbtn = document.getElementById("musicselectionbtn");
 
-
+//settings modal
+var settingmodal = document.getElementById("settings-modal");
+var settingbtn = document.getElementById("settingbtn");
 
 musicselectionbtn.onclick = function(){
     musicmodal.style.display = "block";
+}
+
+settingbtn.onclick = function(){
+    settingmodal.style.display = "block";
 }
 
 
@@ -17,6 +23,9 @@ window.onclick = function(){
     if (event.target == musicmodal) {
        musicmodal.style.display = "none";
    }
+   if (event.target == settingmodal) {
+      settingmodal.style.display = "none";
+  }
 }
 
 
